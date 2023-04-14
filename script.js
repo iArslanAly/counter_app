b9b2df8735e90b393bada016d640630b0f2b23b6
Arslan Ali <aliarslan462@yahoo.com>
2023-04-15 00:26:16 +0500

    save incrementation

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 //document.getElementById("count").innerText = 5
+let saveStr = document.getElementById("countP")
 let countVar = document.getElementById("countVar")
-console.log(countVar)
+
 let count = 0
 function increment() {
     count = count + 1
     countVar.innerText = count
     console.log(count )
+}
+function save(){
+    let countStr=count + "-"
+
+    saveStr.innerText= saveStr.innerText+countStr
+    console.log(count)
 }
