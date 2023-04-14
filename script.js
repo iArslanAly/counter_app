7bc3206e2f9a44fb49fdb75aa27c84531b763473
Arslan Ali <aliarslan462@yahoo.com>
2023-04-14 23:01:40 +0500

    .

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //document.getElementById("count").innerText = 5
-let countVariable = document.getElementById("countVar")
-console.log(countVariable)
+let countVar = document.getElementById("countVar")
+console.log(countVar)
 let count = 0
 function increment() {
     count = count + 1
-    countVariable.innerText = count
+    countVar.innerText = count
     console.log(count )
 }
